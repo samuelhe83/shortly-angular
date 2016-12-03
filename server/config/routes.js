8de9bef2b70b6ecba46d2acbe164260a5340c25f
Hack Reactor Students <accounts@hackreactor.com>
2016-12-02 19:56:27 -0800

    Take out unneeded commeents.

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,6 +1,5 @@
 var linksController = require('../links/linkController.js');
 var userController = require('../users/userController.js');
-var authController = require('../auth/auth.js');
 
 var helpers = require('./helpers.js'); // our custom middleware
 
@@ -18,7 +17,6 @@ module.exports = function (app, express) {
 
   // If a request is sent somewhere other than the routes above,
   // send it through our custom error handler
-  // app.get('/links', linksController)
 
   app.use(helpers.errorLogger);
   app.use(helpers.errorHandler);
